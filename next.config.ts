c2811fcf3dcb2c3e59411b4a6aa7a6fecbf09ccd
ReiDelsAltres <velibecovv@gmail.com>
2024-12-08 16:02:33 +0400

    Uh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "export",
+    reactStrictMode: true
   /* config options here */
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
