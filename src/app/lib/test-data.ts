c1a91eca28a11116cc116bb9da85a18ff64a7e4e
ReiDelsAltres <velibecovv@gmail.com>
2024-12-08 16:15:40 +0400

    Uh

---
+++ src/app/lib/test-data.ts
@@ -0,0 +1,16 @@
+const someones = [
+    {
+        id:1,
+        name: 'John',
+        email: 'john@example.com',
+        password: 'password',
+        status: true,
+    },
+    {
+        id:2,
+        name: 'Petah',
+        email: 'petah@example.com',
+        password: 'passforward',
+        status: false,
+    }
+]
